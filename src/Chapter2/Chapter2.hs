5753cadf01c0e680308f160363a7536efbd96b2c
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-08-07 07:17:40 +0900

    Add Chapter2

--- src/Chapter2/Chapter2.hs
+++ src/Chapter2/Chapter2.hs
@@ -1,1 +1,33 @@
 module Chapter2.Chapter2 where
+
+double x = x + x
+
+quadruple x = double(double x)
+
+factorial n = product[1..n]
+
+average ns = sum ns `div` length ns
+
+n = a `div` length xs
+    where a = 10
+          xs = [1, 2, 3, 4, 5]
+
+last1 :: [a] -> a
+last1 [x] = x
+last1 (x : xs) = last1 xs
+
+last2 :: [a] -> a
+last2 xs = xs !! (length xs - 1)
+
+last3 :: [a] -> a
+last3 xs = head (reverse xs)
+
+init1 :: [a] -> [a]
+init1 [x] = []
+init1 (x : xs) = [x] ++ init1 xs
+
+init2 :: [a] -> [a]
+init2 xs = take (length xs - 1) xs
+
+init3 :: [a] -> [a]
+init3 xs = reverse (drop 1 (reverse xs))
--- test/Chapter2/Chapter2Spec.hs
+++ test/Chapter2/Chapter2Spec.hs
@@ -8,5 +8,33 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-    describe "" $ do
-        it "" $
+    describe "2.4 Haskellプログラム" $ do
+        it "quadruple" $
+            quadruple 10 `shouldBe` 40
+        it "double" $
+            take (double 2) [1, 2, 3, 4, 5, 6] `shouldBe` [1, 2, 3, 4]
+        it "factorial" $
+            factorial 10 `shouldBe` 3628800
+        it "average" $
+            average [1, 2, 3, 4, 5] `shouldBe` 3
+    describe "2.6 練習問題" $ do
+        it "1 - 1" $
+            2 ^ 3 * 4 `shouldBe` (2 ^ 3) * 4 
+        it "1 - 2" $
+            2 * 3 + 4 * 5 `shouldBe` (2 * 3) + (4 * 5)
+        it "1 - 3" $
+            2 + 3 * 4 ^ 5 `shouldBe` 2 + (3 * (4 ^ 5))
+        it "3" $
+            n `shouldBe` 2
+        it "4 - 1" $
+            last1 [1, 2, 3] `shouldBe` 3
+        it "4 - 2" $
+            last2 [1, 2, 3] `shouldBe` 3
+        it "4 - 3" $
+            last3 [1, 2, 3] `shouldBe` 3
+        it "5 - 1" $
+            init1 [1, 2, 3] `shouldBe` [1, 2]
+        it "5 - 2" $
+            init2 [1, 2, 3] `shouldBe` [1, 2]
+        it "5 - 3" $
+            init3 [1, 2, 3] `shouldBe` [1, 2]
