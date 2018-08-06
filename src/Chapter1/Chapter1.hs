d230f0c22357172922b3769648ae52cd0961e0b4
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-08-06 20:19:38 +0900

    Add Chapter1

--- src/Chapter1/Chapter1.hs
+++ src/Chapter1/Chapter1.hs
@@ -1,1 +1,30 @@
-module Chapter1.Chapter1
+module Chapter1.Chapter1 where
+
+double :: Num a => a -> a
+double x = x + x
+
+sum' :: Num a => [a] -> a
+sum' [] = 0
+sum' (x : xs) = x + sum' xs
+
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
+qsort (x : xs) = qsort smaller ++ [x] ++ qsort larger
+    where smaller = [a | a <- xs, a <= x]
+          larger = [b | b <- xs, b > x]
+
+product' :: Num a => [a] -> a
+product' [] = 1
+product' (x : xs) = x * product' xs
+
+qsort' :: Ord a => [a] -> [a]
+qsort' [] = []
+qsort' (x : xs) = qsort' larger ++ [x] ++ qsort' smaller
+    where smaller = [a | a <- xs, a <= x]
+          larger = [b | b <- xs, b > x]
+
+qsort'' :: Ord a => [a] -> [a]
+qsort'' [] = []
+qsort'' (x : xs) = qsort'' smaller ++ [x] ++ qsort'' larger
+    where smaller = [a | a <- xs, a < x]
+          larger = [b | b <- xs, b > x]
--- test/Chapter1/Chapter1Spec.hs
+++ test/Chapter1/Chapter1Spec.hs
@@ -8,5 +8,24 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "" $ do
-    it "" $
+    describe "1.1 関数" $ do
+        it "double" $
+            double 2 `shouldBe` 4
+    describe "1.5 Haskellの妙味" $ do
+        it "sum'" $
+            sum' [1..10] `shouldBe` 55
+        it "qsort" $
+            qsort [3, 5, 1, 4, 2] `shouldBe` [1, 2, 3, 4, 5]
+    describe "1.7 練習問題" $ do
+        it "1" $
+            double(double 2) `shouldBe` double 2 + double 2
+        it "2" $
+            sum [0] `shouldBe` 0
+        it "2-2" $
+            sum [1] `shouldBe` 1
+        it "3" $
+            product' [2, 3, 4] `shouldBe` 24
+        it "4" $
+            qsort' [3, 5, 1, 4, 2] `shouldBe` [5, 4, 3, 2, 1]
+        it "5" $
+            qsort'' [2, 2, 3, 1, 1] `shouldBe` [1, 2, 3]
