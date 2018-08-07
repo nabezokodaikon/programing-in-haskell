a9b38293eb1500e9c28e2678db736b8543552de6
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-08-08 06:20:41 +0900

    Add Chapter3

--- src/Chapter3/Chapter3.hs
+++ src/Chapter3/Chapter3.hs
@@ -1,1 +1,26 @@
 module Chapter3.Chapter3 where
+
+add :: (Int, Int) -> Int
+add (x, y) = x + y
+
+zeroto :: Int -> [Int]
+zeroto n = [0..n]
+
+-- 練習問題 2
+second :: [a] -> a
+second xs = head(tail xs)
+
+swap :: (a, b) -> (b, a)
+swap (x, y) = (y, x)
+
+pair :: a -> b -> (a, b)
+pair x y = (x, y)
+
+double :: Num a => a -> a
+double x = x * 2
+
+palindrome :: Eq a => [a] -> Bool
+palindrome xs = reverse xs == xs
+
+twice :: (a -> a) -> a -> a 
+twice f x = f(f x)
--- test/Chapter3/Chapter3Spec.hs
+++ test/Chapter3/Chapter3Spec.hs
@@ -8,5 +8,20 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-    describe "" $ do
-        it "" $
+    describe "3.5 関数型" $ do
+        it "add" $
+            add (1, 2) `shouldBe` 3
+        it "zeroto" $
+            zeroto 3 `shouldBe` [0, 1, 2, 3]
+    describe "3.11 練習問題" $ do
+        it "1 - 1" $
+            (read "['a', 'b', 'c']" :: [Char]) `shouldBe` ['a', 'b', 'c']
+        it "1 - 2" $
+            (read "('a', 'b', 'c')" :: (Char, Char, Char)) `shouldBe` ('a', 'b', 'c')
+        it "1 - 3" $
+            (read "[(False, '0'), (True, '1')]" :: [(Bool, Char)]) `shouldBe` [(False, '0'), (True, '1')]
+        it "1 - 4" $
+            (read "([False, True], ['0', '1'])" :: ([Bool], [Char])) `shouldBe` ([False, True], ['0', '1'])
+        it "1 - 5" $
+            (read "[[2, 3], [1, 2], [3, 2, 1]]" :: [[Int]]) `shouldBe`
+            [tail [1, 2, 3], init [1, 2, 3], reverse [1, 2, 3]]
