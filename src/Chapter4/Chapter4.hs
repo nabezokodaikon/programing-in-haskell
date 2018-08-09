6f7fa7121ac7c26715e52518402579f90299dea6
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-08-09 20:37:54 +0900

    Add Chapter4

--- src/Chapter4/Chapter4.hs
+++ src/Chapter4/Chapter4.hs
@@ -1,1 +1,45 @@
 module Chapter4.Chapter4 where
+
+halve :: [a] -> ([a], [a])
+halve xs | length xs `mod` 2 == 0 = (take (f xs) xs, drop (f xs) xs)
+         | otherwise = ([], [])
+    where f xs = length xs `div` 2
+
+safetail :: [a] -> [a]
+safetail [] = []
+safetail (_ : xs) = xs
+
+safetail2 :: [a] -> [a]
+safetail2 xs = if null xs
+                  then []
+                  else tail xs
+
+safetail3 :: [a] -> [a]
+safetail3 xs 
+    | null xs = []
+    | otherwise = tail xs
+
+orElse :: Bool -> Bool -> Bool
+orElse False False = False
+orElse _ _ = True
+
+orElse2 :: Bool -> Bool -> Bool
+orElse2 x y = if x == False && y == False
+                 then False
+                 else True
+
+orElse3 :: Bool -> Bool -> Bool
+orElse3 x y | x == False && y == False = False
+            | otherwise = True
+
+andAlso :: Bool -> Bool -> Bool
+andAlso x y = if x == True
+                 then 
+                    if y == True
+                    then True
+                    else False
+                 else False
+
+mult :: Int -> Int -> Int -> Int
+mult a b c = f a b c
+    where f = (\x -> \y -> \z -> x * y * z)
--- test/Chapter4/Chapter4Spec.hs
+++ test/Chapter4/Chapter4Spec.hs
@@ -8,6 +8,24 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-    describe "" $ do
-        it "" $
-
+    describe "4.8 練習問題" $ do
+        it "1" $
+            halve [1, 2, 3, 4, 5, 6] `shouldBe` ([1, 2, 3], [4, 5, 6])
+        it "2" $
+            safetail [1, 2, 3] `shouldBe` [2, 3]
+        -- it "2 - 2" $
+            -- safetail [] `shouldBe` []
+        it "3" $
+            True `orElse` True `shouldBe` True
+        it "3 - 2" $
+            True `orElse2` False `shouldBe` True
+        it "3 - 3" $
+            False `orElse3` True `shouldBe` True
+        it "3 - 4" $
+            False `orElse` False `shouldBe` False 
+        it "4" $
+            True `andAlso` True `shouldBe` True
+        it "4 - 2" $
+            True `andAlso` False `shouldBe` False
+        it "6" $
+            mult 2 3 4 `shouldBe` 24
