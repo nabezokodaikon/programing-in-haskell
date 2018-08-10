da3202998cf9b69d65c2ab8b1db4d7b8a3efbd76
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-08-10 18:38:16 +0900

    Add 6.1 基本概念

--- src/Chapter6/Chapter6.hs
+++ src/Chapter6/Chapter6.hs
@@ -1,1 +1,9 @@
 module Chapter6.Chapter6 where
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+multiple :: Int -> Int -> Int
+multiple n 0 = 0
+multiple n m = n + (n `multiple` (m - 1))
--- test/Chapter6/Chapter6Spec.hs
+++ test/Chapter6/Chapter6Spec.hs
@@ -8,5 +8,12 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-    describe "" $ do
-        it "" $
+    describe "6.1 基本概念" $ do
+        it "factorial" $
+            factorial 3 `shouldBe` 6
+        it "multiple" $
+            3 `multiple` 5 `shouldBe` 15
+        it "multiple 2" $
+            3 `multiple` 1 `shouldBe` 3
+        it "multiple 3" $
+            3 `multiple` 0 `shouldBe` 0
