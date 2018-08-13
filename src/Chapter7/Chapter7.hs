709085fed4f7c07b588b3c4af9306cfd45848adf
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-08-13 16:54:10 +0900

    Add 7.4 畳込関数foldl

--- src/Chapter7/Chapter7.hs
+++ src/Chapter7/Chapter7.hs
@@ -1,1 +1,51 @@
 module Chapter7.Chapter7 where
+
+add :: Int -> (Int -> Int)
+add = \x -> (\y -> x + y)
+
+twice :: (a -> a) -> a -> a
+twice f x = f(f(x))
+
+sumsqreven :: [Int] -> Int
+sumsqreven ns = sum (map (^2) (filter even ns))
+
+foldr' :: (a -> b -> b) -> b -> [a] -> b
+foldr' f v [] = v
+foldr' f v (x : xs) = f x (foldr' f v xs)
+
+sum' :: Num a => [a] -> a
+sum' [] = 0
+sum' xs = foldr' (+) 0 xs
+
+sum'' :: Num a => [a] -> a
+sum'' = foldr' (+) 0
+
+length' :: [a] -> Int
+length' [] = 0
+length' (_ : xs) = 1 + length' xs
+
+length'' :: [a] -> Int
+length'' = foldr (\_ n -> 1 + n) 0
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x : xs) = reverse xs ++ [x]
+
+snoc :: a -> [a] -> [a] 
+snoc x xs = xs ++ [x]
+
+reverse'' :: [a] -> [a]
+reverse'' [] = []
+reverse'' (x : xs) = snoc x (reverse'' xs)
+
+reverse''' :: [a] -> [a]
+reverse''' = foldr snoc []
+
+sum2 :: [Int] -> Int
+sum2 = sum' 0
+    where
+        sum' v [] = v
+        sum' v (x : xs) = sum'(v + x) xs
+
+sumViaFoldl :: [Int] -> Int
+sumViaFoldl = foldl (+) 0
--- test/Chapter7/Chapter7Spec.hs
+++ test/Chapter7/Chapter7Spec.hs
@@ -8,5 +8,23 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-    describe "" $ do
-        it "" $
+    describe "7.1 基本概念" $ do
+        it "twice" $
+            twice (* 2) 3 `shouldBe` 12
+        it "twice 2" $
+            twice reverse [1, 2, 3] `shouldBe` [1, 2, 3]
+    describe "7.2 リスト処理" $ do
+        it "sumsqreven" $
+            sumsqreven [1, 2, 3, 4] `shouldBe` 20
+    describe "7.3 畳込関数foldr" $ do
+        it "sum''" $
+            sum'' [1, 2, 3] `shouldBe` 6
+        it "length''" $
+            length'' [1, 2, 3] `shouldBe` 3
+        it "reverse'''" $
+            reverse''' [1, 2, 3] `shouldBe` [3, 2, 1]
+    describe "7.4 畳込関数foldl" $ do
+        it "sum2" $
+            sum2 [1, 2, 3] `shouldBe` 6
+        it "sumViaFoldl" $
+            sumViaFoldl [1, 2, 3] `shouldBe` 6
